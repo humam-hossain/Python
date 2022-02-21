718d7cb91820c493b460585c5674096400c0115c
humam-hossain <humam.hossain.e@gmail.com>
2022-02-21 23:13:40 +0600

    updated on Mon Feb 21 11:13:40 PM +06 2022

--- opencv/read.py
+++ opencv/read.py
@@ -1,20 +1,20 @@
 import cv2 as cv
 
-# img = cv.imread("/home/painkiller/github_repo/Python/opencv/Photos/cat_large.jpg")
+img = cv.imread("/home/painkiller/github_repo/Python/opencv/Photos/cat_larges.jpg")
 
-# cv.imshow("Cat", img)
+cv.imshow("Cat", img)
 
-# cv.waitKey(0)
+cv.waitKey(0)
 
 # Reading videos
-capture = cv.VideoCapture("/home/painkiller/github_repo/Python/opencv/Videos/dog.mp4")
+# capture = cv.VideoCapture("/home/painkiller/github_repo/Python/opencv/Videos/dog.mp4")
 
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow("Video", frame)
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow("Video", frame)
 
-    if cv.waitKey(20) & 0xFF ==ord('d'):
-        break
+#     if cv.waitKey(20) & 0xFF ==ord('d'):
+#         break
 
-capture.release()
-cv.destroyAllWindows()
+# capture.release()
+# cv.destroyAllWindows()
